aa76a82ccd293ca88dae188c28533d826e219f88
Nikhil-K09 <kalavaguntanikhil@gmail.com>
2025-05-28 16:26:49 +0530

    working buttons

--- app/src/main/java/com/example/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/example/simplecalculator/MainActivity.kt
@@ -2,12 +2,18 @@ package com.example.simplecalculator
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var workingsTV:TextView
+    private lateinit var resultsTV:TextView
+    private var canAddOperation=false
+    private var canAddDecimal=true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,9 +23,51 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        workingsTV=findViewById(R.id.workingsTV)
+        resultsTV=findViewById(R.id.resultsTV)
+    }
+    fun numberAction(view: View){
+        if(view is Button){
+            if(view.text=="."){
+                if(canAddDecimal)
+                    workingsTV.append(view.text)
+                canAddDecimal=false
+            }else{
+                workingsTV.append(view.text)
+            }
+            canAddOperation=true
+        }
+    }
+    fun operatorAction(view: View){
+        if(view is Button && canAddOperation){
+            workingsTV.append(view.text)
+            canAddOperation=false
+            canAddDecimal=true
+        }
+    }
+    fun allClearAction(view: View) {
+        workingsTV.text=""
+        resultsTV.text=""
+    }
+    fun backSpaceAction(view: View) {
+        val length =workingsTV.length()
+        if(length>0){
+            workingsTV.text=workingsTV.text.subSequence(0,length-1)
+        }
+        canAddOperation=true
+    }
+    fun equalsAction(view: View) {
+        resultsTV.text=calculateResults()
     }
 
-    fun allClearAction(view: View) {}
-    fun backSpaceAction(view: View) {}
-    fun equalsAction(view: View) {}
+    private fun calculateResults(): String {
+        return ""
+    }
+    private fun digitsOperators():MutableList<Any>{
+        val list= mutableListOf<Any>()
+        var currentDigit=""
+
+        return list
+    }
+
 }
